8ad672f194ff7e3d17ac4c65a12795a7dfec1349
Denis <Denis_bc@ukr.net>
2016-04-24 11:05:20 +0300

    All lessons were download for Y F course

---
+++ YF_Unit5_HelloWorld/src/MainWindow.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class MainWindow extends  JFrame {
+	public MainWindow() {
+		// TODO Auto-generated constructor stub
+		setVisible(true);
+		setSize(400, 500);
+		
+	}
+}
---
+++ YF_Unit5_HelloWorld/src/StartApp.java
@@ -0,0 +1,11 @@
+
+public class StartApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MainWindow app=new MainWindow();
+		app.setTitle("Helloworld GUI Program");
+				
+	}
+
+}
