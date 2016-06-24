ccb8bbb7fdad6577e9025cf5bc57a8cf20100821
Denis Lebedev <denis_bc@ukr.net>
2016-06-24 07:23:37 +0300

    lesson14 was started

---
+++ lesson14/src/Test.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		ArrayList customers = new ArrayList();
+		Customer cust1 = new Customer("David","Lee");
+		customers.add(cust1);
+		Customer cust2 = new Customer("Ringo","Starr");
+		customers.add(cust2);
+		
+		Customer theBestCustomer = (Customer) customers.get(0);
+		
+		Order ord=new Order();
+		customers.add(ord);
+		
+		int totalElem=customers.size();
+		
+		for (int i=0; i<totalElem; i++){
+			
+			Customer currentCustomer=(Customer) customers.get(i);
+		}
+		
+	}
+
+}
